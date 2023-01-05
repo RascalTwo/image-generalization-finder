11ea87f698fed5e3569bad12917e6e3a723008a3
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-05 15:03:11 -0500

    Remove morgan (again)

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const morgan = require('morgan')
 const app = express();
-app.use(morgan('dev'))
 app.get("/", (req, res) => {
   res.send('Hello, Cyclic!');
 });
