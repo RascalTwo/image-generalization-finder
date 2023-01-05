c4ad5eb1b98d8b90ff0af06f97838268df59a86a
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-05 14:39:46 -0500

    Cyclic NPM bug MVP

--- backend/server.js
+++ backend/server.js
@@ -1,60 +1,8 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const passport = require("passport");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")
-const logger = require("morgan");
-
-const connectDB = require("./config/database");
-const mainRoutes = require("./routes/main");
-const BadImagesRouter = require("./routes/BadImagesRouter");
-const GoodImagesRouter = require("./routes/GoodImagesRouter");
-const CustomQueryRouter = require("./routes/CustomQueryRouter");
-//Use .env file in config folder
-require("dotenv").config({ path: "./config/.env" });
-
-// Passport config
-require("./config/passport")(passport);
-
-//Using EJS for views
-app.set("view engine", "ejs");
-
-//Static Folder
-app.use(express.static("public"));
-
-//Body Parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//Logging
-app.use(logger("dev"));
-
-//Connect To Database
-connectDB().then(conn => {
-  // Setup Sessions - stored in MongoDB
-  app.use(
-    session({
-      secret: "keyboard cat",
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({ client: mongoose.connection.getClient() }),
-    })
-  );
-
-  // Passport middleware
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  //Setup Routes For Which The Server Is Listening
-  app.use("/api/", mainRoutes);
-  app.use("/api/BadImages", BadImagesRouter);
-  app.use("/api/GoodImages", GoodImagesRouter);
-  app.use("/api/custom-query", CustomQueryRouter);
-
-
-  //Server Running
-  app.listen(process.env.PORT, () => {
-    console.log("Server is running, you better catch it!");
-  });
+app.get("/", (req, res) => {
+  res.send('Hello, Cyclic!');
+});
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
 });
